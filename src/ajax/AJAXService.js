053608f5fb20301238025f3a597481a42cdadbd0
Ruben Moros <ruben.moros@outlook.com>
2025-06-02 18:34:10 +0200

    Added to Ajaxservice

--- src/ajax/AJAXService.js
+++ src/ajax/AJAXService.js
@@ -1,21 +1,25 @@
 import ProductAJAX from './ProductAJAX';
 import PersonAJAX from './PersonAJAX';
 import EntityAJAX from './EntityAJAX';
+import AssociationAJAX from './AssociationAJAX';
 
 class AJAXService {
     static initialize() {
         const productAJAX = new ProductAJAX();
         const personAJAX = new PersonAJAX();
         const entityAJAX = new EntityAJAX();
+        const associationAJAX = new AssociationAJAX();
 
         productAJAX.setAJAXInstances(personAJAX, entityAJAX);
         personAJAX.setAJAXInstances(productAJAX, entityAJAX);
         entityAJAX.setAJAXInstances(productAJAX, personAJAX);
+        associationAJAX.setAJAXInstances(productAJAX, personAJAX, entityAJAX);
 
         return {
             productAJAX,
             personAJAX,
-            entityAJAX
+            entityAJAX,
+            associationAJAX
         };
     }
 }
